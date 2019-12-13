d3a64fa042c71cec2826196c5498704a14e23117
Mahesh Srinivas <mahsrin1@in.ibm.com>
2019-12-13 13:54:35 +0530

    Refactored appsody to different namespace to test tektron pieline

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(Main.class, args);
+	}
+
+}
