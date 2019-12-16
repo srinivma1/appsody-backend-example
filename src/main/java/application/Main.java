0e8abe75ece62ef510ac2a935a7f5b509504276a
Mahesh Srinivas <maheshsbrmnn@gmail.com>
2019-12-16 19:59:22 +0530

    Update Main.java

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World mahesh");
 		SpringApplication.run(Main.class, args);
 	}
 
